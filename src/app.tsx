56b8ec4a86d89dd604ccb87b27f88219c8baef7b
mazeltovik <mazeltovik@gmail.com>
2024-08-04 12:11:36 +0300

    feat: add basic timePeriod component

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import RingMenu from './components/ringMenu/ringMenu';
+import TimePeriod from './components/timePeriod/timePeriod';
 
 export const App = () => {
-  return <RingMenu/>;
+  return <TimePeriod/>;
 };
---
+++ src/components/timePeriod/timePeriod.tsx
@@ -0,0 +1,29 @@
+//Styles
+import './timePeriod.scss';
+
+//React
+import { useState, useEffect } from 'react';
+
+//Handlers
+
+
+export default function TimePeriod(){
+    const [period1, setPeriod1] = useState(1997);
+    const [period2, setPeriod2] = useState(2003);
+    const dest = [2010,2025]
+    useEffect(()=>{
+        const [dest1, dest2] = dest;
+        if(period1 != dest1 && period2 != dest2){
+            setTimeout(()=>{
+                setPeriod1(period1 + 1);
+                setPeriod2(period2 + 1);
+            },100);
+        }
+    },[period1,period2])
+    return (
+        <div className='timeContainer'>
+            <p>{period1}</p>
+            <p>{period2}</p>
+        </div>
+    )
+}
